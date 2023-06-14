d1c2e4ca2e9afca310f82ac91ab1a50b2c1a957c
Lydiacwall <88028518+Lydiacwall@users.noreply.github.com>
2023-06-14 12:20:44 +0300

    created the result and leaberboard layout

--- app/src/main/java/com/example/pacepal/view/sender/SenderFragment.java
+++ app/src/main/java/com/example/pacepal/view/sender/SenderFragment.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.os.Environment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
+
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
